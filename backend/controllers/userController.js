abdf9967eb3b47f369194969a426248df09a8723
BWhittaker1415 <bwhittaker1415@gmail.com>
2023-11-19 23:17:56 -0500

    Finished controllers, Hashed/Salted passwords

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 // ================== AUTH USER ================== //
@@ -6,7 +7,21 @@ import User from "../models/userModel.js";
 // route   =>  POST /api/users/auth
 // access   =>   Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // ================== REGISTER USER ================== //
@@ -31,12 +46,14 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
     });
   } else {
+    res.status(400);
     throw new Error("Invalid user data");
   }
 });
@@ -46,7 +63,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // route   =>  POST /api/users/logout
 // access   =>   Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "logout User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User is logged out" });
 });
 
 // ================== GET USER PROFILE ================== //
@@ -54,7 +76,13 @@ const logoutUser = asyncHandler(async (req, res) => {
 // route   =>  GET /api/users/profile
 // access   =>   Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
 });
 
 // ================== UPDATE USER PROFILE ================== //
@@ -62,7 +90,27 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // route   =>  PUT /api/users/profile
 // access   =>   Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update user profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,7 +28,23 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
   }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("confirmPassword")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.confirmPassword = await bcrypt.hash(this.confirmPassword, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
